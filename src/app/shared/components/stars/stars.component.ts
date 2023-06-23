2a4b0fd3ed4b8e7ce8168e4412a961c7ed9328a7
unknown <frederic.lossignol@gmail.com>
2023-06-23 12:13:40 +0200

    components app-stars solution Template in TS

--- src/app/shared/components/stars/stars.component.ts
+++ src/app/shared/components/stars/stars.component.ts
@@ -1,19 +1,30 @@
 import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-stars',
-  templateUrl: './stars.component.html',
+  template: `
+  <!-- <i *ngFor="let item of scoreArr" class="fas fa-star"></i> -->
+  <div [innerHTML]="iconsStr"></div>
+  `,
   styleUrls: ['./stars.component.scss']
 })
 export class StarsComponent {
 
   @Input() score!: number;
-  scoreArr: Array<number> = []
+  iconsStr: string = '';
+  //scoreArr: Array<number> = []
 
   ngOnInit() {
-    this.score = Math.round(this.score / 2)
-    this.scoreArr = new Array(this.score).fill(0); // [0,0,0] si score=3
 
+    this.score = Math.round(this.score / 2)
+    //this.scoreArr = new Array(this.score).fill(0); // [0,0,0] si score=3
+    for (let i = 0; i < this.score; i++) {
+      this.iconsStr += '<i style="color:orange" class="fas fa-star"></i>'
+    }
+    for (let i = this.score; i < 5; i++) {
+      this.iconsStr += '<i style="color:orange"class="far fa-star"></i>'
+    }
   }
 
 }
